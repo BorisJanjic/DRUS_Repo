28cdb719941160d6f0d3d710476b684123a599b8
Boris Janjic <borisjanjic5@gmail.com>
2018-05-03 23:41:46 +0200

    Measurement service with database

---
+++ MeasurementService/MeasurementService/IMeasurementService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MeasurementService
+{
+    
+    [ServiceContract(CallbackContract = typeof(IMeasurementServiceCallback))]
+    public interface IMeasurementService
+    {
+        [OperationContract(IsOneWay =true)]
+        void Subscribe(int id);
+
+        [OperationContract(IsOneWay =true)]
+        void Unsubscribe(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void UnsubscribeAll(int id);
+
+        [OperationContract]
+        void AddMeasurement(int id, int value, string type);
+
+        [OperationContract]
+        String ViewActiveStations();
+
+        [OperationContract]
+        String GetAllMeasurementsOfMeasurer(int measurerId, DateTime fromTime, DateTime toTime, int type);
+        [OperationContract]
+        String GetAllMomentsOfLimitValues(int measurerId, int type, int limitType, double limit);
+
+        [OperationContract]
+        string GetAllMomentsOfLimitValuesOnSpecificLocation(String locationName, int type, int limitType, double limit);
+
+        [OperationContract]
+        String GetAverageOnLocation(String locationName, int type, DateTime dateFrom, DateTime dateUntil);
+    }
+
+    public interface IMeasurementServiceCallback
+    {
+        [OperationContract]
+        void NotifyOfMeasurement(int id, double value, string type);
+    }
+    
+}
