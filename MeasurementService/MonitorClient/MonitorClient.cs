558eb1ea5ec60d3b80357805043be84755b1e01d
Boris Janjic <borisjanjic5@gmail.com>
2018-05-11 18:39:26 +0200

    Refactoring Monitor Client

--- MeasurementService/MonitorClient/MonitorClient.cs
+++ MeasurementService/MonitorClient/MonitorClient.cs
@@ -48,124 +48,35 @@ namespace MonitorClient
                         break;
                     case "3":
                         {
-                            Console.WriteLine("Type in station id");
-
-                            String stationId = Console.ReadLine();
-                            int station = int.Parse(stationId);
-                       
-                            Console.WriteLine("Type in from date format [dd/mm/yyyy hh:mm:ss.stst]");
-
-                            String dateFromStr = Console.ReadLine();
-                            DateTime df = ConvertStringToDate(dateFromStr);
-                          
-                            Console.WriteLine("Type in until date format [dd/mm/yyyy hh:mm:ss.stst]");
-                            String dateUntilStr = Console.ReadLine();
-                            DateTime du = ConvertStringToDate(dateUntilStr);
-
-                            DisplayMeasurementsFromStation(station, df, du, 0);
+                            StationMeasurement(0);
                         }
                         break;
                     case "4":
                         {
-                            Console.WriteLine("Type in station id");
-
-                            String stationId = Console.ReadLine();
-                            int station = int.Parse(stationId);
-                            //from
-                            Console.WriteLine("Type in from date format [dd/mm/yyyy hh:mm:ss.stst]");
-
-                            String dateFromStr = Console.ReadLine();
-                            DateTime df = ConvertStringToDate(dateFromStr);
-                            ///until
-                            Console.WriteLine("Type in until date format [dd/mm/yyyy hh:mm:ss.stst]");
-                            String dateUntilStr = Console.ReadLine();
-                            DateTime du = ConvertStringToDate(dateUntilStr);
-
-
-                            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature\n 0=Both");
-                            String typeStr = Console.ReadLine();
-                            int type = int.Parse(typeStr);
-
-
-                            Console.Write(client.GetAllMeasurementsOfMeasurer(station, df, du, type));
+                            StationMeasurement(1);
                         }
                         break;
                     case "5":
                         {
-                            Console.WriteLine("Available stations are: \n" + client.ViewActiveStations());                           
+                            Console.WriteLine("Available stations are: \n" + client.ViewActiveStations());
+                            DisplayOptions();
                         }
                         break;
                     case "6":
                         {
-                            Console.WriteLine("Type in station id");
-
-                            String stationId = Console.ReadLine();
-                            int station = int.Parse(stationId);
-                            //from
-                            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature");
-
-                            String typeStr = Console.ReadLine();
-                            int type = int.Parse(typeStr);
-
-                            Console.WriteLine("Type in limit type \n 1 = < \n 2 ");
-
-                            String limitType = Console.ReadLine();
-                            int limitSign = int.Parse(limitType);
-
-                            Console.WriteLine("Type in limit number");
-
-                            String limitNumber = Console.ReadLine();
-
-                            double limit = double.Parse(limitNumber);
-
-                            Console.WriteLine(client.GetAllMomentsOfLimitValues(station, type, limitSign, limit));
+                            LimitMoments(0);
                         }
                         break;
                     case "7":
                         {
-                            Console.WriteLine("Type in location name");
-
-                            String location = Console.ReadLine();
-
-                            //from
-                            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature");
-
-                            String typeStr = Console.ReadLine();
-                            int type = int.Parse(typeStr);
-
-                            Console.WriteLine("Type in limit type \n 1 = < \n 2 ");
-
-                            String limitType = Console.ReadLine();
-                            int limitSign = int.Parse(limitType);
-
-                            Console.WriteLine("Type in limit number");
-
-                            String limitNumber = Console.ReadLine();
-
-                            double limit = double.Parse(limitNumber);
-
-                            Console.Write(client.GetAllMomentsOfLimitValuesOnSpecificLocation(location, type, limitSign, limit));
+                            LimitMoments(1);
                         }
                         break;
                     case "8":
                         {
 
-                            Console.WriteLine("Type in location name");
-                            String location = Console.ReadLine();
-                            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature");
+                            LimitMoments(2);
 
-                            String typeStr = Console.ReadLine();
-                            int type = int.Parse(typeStr);
-
-                            Console.WriteLine("Type in from date (format [dd/mm/yyyy hh:mm:ss.stst])");
-                            String dateFromStr = Console.ReadLine();
-                            DateTime df = ConvertStringToDate(dateFromStr);
-                            ///until
-                            Console.WriteLine("Type in until date (format [dd/mm/yyyy hh:mm:ss.stst])");
-                            String dateUntilStr = Console.ReadLine();
-                            DateTime du = ConvertStringToDate(dateUntilStr);
-
-                            DisplayAverages(type, location, df, du);
                         }
                         break;
                     case "9":
@@ -188,6 +99,95 @@ namespace MonitorClient
 
             }
 
+        }
+        
+        private void StationMeasurement(int i)
+        {
+            Console.WriteLine("Type in station id");
+
+            String stationId = Console.ReadLine();
+            int station = int.Parse(stationId);
+
+            Console.WriteLine("Type in from date format [dd/mm/yyyy hh:mm:ss.stst]");
+
+            String dateFromStr = Console.ReadLine();
+            DateTime df = ConvertStringToDate(dateFromStr);
+
+            Console.WriteLine("Type in until date format [dd/mm/yyyy hh:mm:ss.stst]");
+            String dateUntilStr = Console.ReadLine();
+            DateTime du = ConvertStringToDate(dateUntilStr);
+
+            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature\n 0=Both");
+            String typeStr = Console.ReadLine();
+            int type = int.Parse(typeStr);
+
+            if (i==0)
+                DisplayMeasurementsFromStation(station, df, du, 0);
+            else
+                DisplayMeasurementsFromStation(station, df, du, type);
+
+            DisplayOptions();
+        }
+
+        private void LimitMoments(int i)
+        {
+            int station = 0;
+            String location = "";
+            double limit = 0.0;
+            int limitSign = 0;
+
+            if (i == 0)
+            {
+                Console.WriteLine("Type in station id");
+
+                String stationId = Console.ReadLine();
+                station = int.Parse(stationId);    
+            }
+            else
+            {
+                Console.WriteLine("Type in location name");
+
+                location = Console.ReadLine();
+            }
+            //from
+            Console.WriteLine("Type in type of measurement \n 1=Humidity\n 2=Temperature");
+
+            String typeStr = Console.ReadLine();
+            int type = int.Parse(typeStr);
+
+            if (i < 2)
+            {
+                Console.WriteLine("Type in limit type \n 1 = < \n 2 ");
+
+                String limitType = Console.ReadLine();
+                limitSign = int.Parse(limitType);
+
+                Console.WriteLine("Type in limit number");
+
+                String limitNumber = Console.ReadLine();
+                limit = double.Parse(limitNumber);
+                if (i == 0)
+                {
+                    Console.WriteLine(client.GetAllMomentsOfLimitValues(station, type, limitSign, limit));
+                }
+                else
+                {
+                    Console.Write(client.GetAllMomentsOfLimitValuesOnSpecificLocation(location, type, limitSign, limit));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Type in from date (format [dd/mm/yyyy hh:mm:ss.stst])");
+                String dateFromStr = Console.ReadLine();
+                DateTime df = ConvertStringToDate(dateFromStr);
+                //until
+                Console.WriteLine("Type in until date (format [dd/mm/yyyy hh:mm:ss.stst])");
+                String dateUntilStr = Console.ReadLine();
+                DateTime du = ConvertStringToDate(dateUntilStr);
+
+                DisplayAverages(type, location, df, du);
+            }            
+            DisplayOptions();
         }
 
         private void DisplayMeasurementsFromStation(int station, DateTime df, DateTime du, int v)
